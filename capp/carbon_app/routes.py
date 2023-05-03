5c92e005226f5337d611e617f049c98396477c70
theoyster1 <125879588+theoyster1@users.noreply.github.com>
2023-05-03 10:40:01 +0200

    Update routes.py

--- capp/carbon_app/routes.py
+++ capp/carbon_app/routes.py
@@ -52,7 +52,7 @@ def carbon_app_home():
 @login_required
 def your_data():
     if len(Transport.query.filter_by(author = current_user).all()) < 1:
-        return redirect(url_for('carbon_app.carbon_app_home'))
+        return redirect(url_for('carbon_app.carbon_app_home', msg = 'No data'))
 
     # Table
     entries = (
